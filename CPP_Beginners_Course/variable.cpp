7797e2c638cf1607289e999e1c80ff4f1b38825b
Miguelangel Diaz <spalduing12@gmail.com>
2020-09-28 16:39:15 -0500

    variable.cpp file added, explain the interaction between a variable and the cout key word for outputing the content of that variable.

---
+++ CPP_Beginners_Course/variable.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number_dogs = 5;
+    int number_cats = 6;
+
+    cout<< "Number of dogs: " << number_dogs;
+    cout<< "    Number of cats: " << number_cats<< endl;
+
+    cout << "The total number of animals is: " << number_dogs + number_cats << endl;
+}
