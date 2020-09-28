6e0e2ca4e62210c54efdb6341fc1bc37934a2336
Miguelangel Diaz <spalduing12@gmail.com>
2020-09-28 17:53:11 -0500

    strings.cpp file added, it explains the basics of string variables in c++

---
+++ CPP_Beginners_Course/strings.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string text = "hello ";
+    string text2 = "I'm a string";
+
+    string text3 = text + text2;
+
+    cout << text << text2 << endl;
+    cout << text3 << endl;
+}
