e71b5cea56f74285bf73e0389ef56d5fcf72bb45
unknown <spalduing12@gmail.com>
2020-10-05 15:48:46 -0500

    while_loop.cpp file added, it explains how the while loop works

---
+++ CPP_Beginners_Course/while_loop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 0;
+
+    while( i < 5){
+        cout << "hello " << i << endl;
+        //i = i + 1;
+        i++;
+    }
+
+    cout << "Program quiting..." << endl;
+    return 0;
+}
