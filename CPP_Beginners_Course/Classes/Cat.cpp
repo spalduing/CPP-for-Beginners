a122fac0eadb79ded759d91aef51ac7873f41539
Miguelangel Diaz <spalduing12@gmail.com>
2020-10-12 13:10:05 -0500

    Classes c++ project added, it explains how to create classes in c++

---
+++ CPP_Beginners_Course/Classes/Cat.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Cat.h"
+using namespace std;
+
+void Cat::speak()
+{
+    cout << "Meouwww!!" << endl;
+}
+
+void Cat::jump()
+{
+    cout << "*Jumping to the bookcase*" << endl;
+}
