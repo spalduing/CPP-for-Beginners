41de0c88f1e2d9f4df67c8fb575a0db68c7a2821
Miguelangel Diaz <spalduing12@gmail.com>
2020-10-12 11:54:16 -0500

    headers_and_prototypes.cpp and utils.h files added, they explains how to prototype functions or use headers in c++ both in order to have a way of put code bellow of the main method in a c++ program

---
+++ CPP_Beginners_Course/headers_and_prototypes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "utils.h"
+using namespace std;
+
+//void doSomething()
+
+int main()
+{
+    doSomething();
+    doSomething();
+
+    return 0;
+}
+
+void doSomething()
+{
+    cout << "Hello" << endl;
+}
