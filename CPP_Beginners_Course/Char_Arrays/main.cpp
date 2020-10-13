dc765d80f98af84a9620680d51280a4c8894d568
Miguelangel Diaz <spalduing12@gmail.com>
2020-10-13 15:37:45 -0500

    Char_Arrays project added, it explains some little details that you shouldknow and take care when you gonna iterate through an char array en c++

---
+++ CPP_Beginners_Course/Char_Arrays/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    char text[] = "hello";
+    int s = sizeof(text);
+    cout<< text << endl;
+
+    for(int i=0; i<s; i++){
+        if(i == s-1){
+            cout<<i << ":"<< (int)text[i];
+            cout<<flush <<" This extra positions is here to let know where the array ends. It actually represents a 0";
+        }
+        cout<<i << ":"<< text[i]<<endl;
+    }
+
+
+    cout<<endl<<endl;
+
+    int k = 0;
+    while(true){
+        if(text[k] == 0){break;}
+        cout << text[k] << flush;
+        k++;
+    }
+
+    return 0;
+}
