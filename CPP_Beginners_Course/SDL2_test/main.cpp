28f440204da41d6d3bdfd6b5e36a73247914ffff
Miguelangel Diaz <spalduing12@gmail.com>
2020-10-18 12:45:14 -0500

    Creating an SDL Window

--- CPP_Beginners_Course/SDL2_test/main.cpp
+++ CPP_Beginners_Course/SDL2_test/main.cpp
@@ -4,13 +4,43 @@ using namespace std;
 
 int main(int argc, char *argv[] )
 {
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HIGH = 600;
     if( SDL_Init(SDL_INIT_VIDEO) < 0)
     {
         cout << "SDL could not initialise! SDL Error: " << SDL_GetError() << endl;
         return 1;
     }
-    cout << "SDL Init succeeded." << endl;
-    SDL_Quit();
+    SDL_Window *window = SDL_CreateWindow(/*Name*/"Particle Fire Explosion",
+                                          /*x position*/SDL_WINDOWPOS_UNDEFINED,
+                                          /*y position*/SDL_WINDOWPOS_UNDEFINED,
+                                          /*Width*/SCREEN_WIDTH, SCREEN_HIGH,
+                                          /*High*/SDL_WINDOW_SHOWN);
 
-    return EXIT_SUCCESS ;
+
+    if(window == NULL)
+    {
+        SDL_Quit();
+        return 2;
+    }
+
+    bool quit = false;
+
+    SDL_Event event;
+    while(!quit)
+    {
+        // Update particles
+        // Draw particles
+        // Check for messages/events
+        while(SDL_PollEvent(&event))
+        {
+            if(event.type == SDL_QUIT)
+            {
+                quit = true;
+            }
+        }
+    }
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }
