60f0b29dba15ad5059474e0eba9a3b35c48ac1b0
Miguelangel Diaz <spalduing12@gmail.com>
2020-10-18 18:52:32 -0500

    Animating Colors

--- CPP_Beginners_Course/Particle_Fire_Explosion/main.cpp
+++ CPP_Beginners_Course/Particle_Fire_Explosion/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 //#include <SDL2/SDL.h>
+#include <math.h>
 #include "screen.h"
 using namespace std;
 using namespace spalduing;
@@ -12,17 +13,24 @@ int main(int argc, char *argv[] )
 
     bool quit = false;
 
+
     SDL_Event event;
     while(!quit)
     {
         // Update particles
 
+
         // Draw particles
+        int elapsed = SDL_GetTicks();
+        unsigned char green = (unsigned char)((1 + sin(elapsed * 0.001) ) * 128);
+        unsigned char red = (unsigned char)((1 + sin(elapsed * 0.002) ) * 128);
+        unsigned char blue = (unsigned char)((1 + sin(elapsed * 0.003) ) * 128);
+
         for(int y=0; y<Screen::SCREEN_HIGH; y++)
         {
             for(int x=0; x<Screen::SCREEN_WIDTH; x++)
             {
-                screen.setPixel(x, y, 128, 0, 255);
+                screen.setPixel(x, y, red, green, blue);
             }
         }
 
@@ -37,6 +45,7 @@ int main(int argc, char *argv[] )
     }
 
     screen.close();
+
 
     return 0;
 }
