69f5005cd1ba8cb11775f4b12fb2debc12d661c5
Miguelangel Diaz <spalduing12@gmail.com>
2020-10-19 10:26:42 -0500

    Bitwise And

--- CPP_Beginners_Course/Bitwise_AND/main.cpp
+++ CPP_Beginners_Course/Bitwise_AND/main.cpp
@@ -11,22 +11,28 @@ int main()
     *   so only the bits corresponding
     *   to red will remain.
     */
-    unsigned int red = color & 0xFF0000;
-    cout<<"red:     "<< setfill('0') << setw(6) << hex <<red<<endl;
+    unsigned /*int*/char red = (color & 0xFF0000) >> 16;
+    //unsigned /*int*/char red = color  >> 16;
 
-    unsigned int green = color & 0x00FF00;
-    cout<<"green:   "<< setfill('0') << setw(6) << hex <<green<<endl;
+    unsigned /*int*/char green = (color & 0x00FF00) >> 8;
+    //unsigned /*int*/char green = color >> 8;
 
-    unsigned int blue = color & 0x0000FF;
-    cout<<"blue:    "<< setfill('0') << setw(6) << hex <<blue<<endl;
+    unsigned /*int*/char blue = (color & 0x0000FF);
+    //unsigned /*int*/char blue = color;
 
     //  color: 100010111101111011111011
     //  mask:  111111110000000000000000
     //  result:100010110000000000000000
 
+    cout<<"red:     " << hex <<( int)red<<endl;
+    cout<<"green:   " << hex << (int)green<<endl;
+    cout<<"blue:    " << hex << (int)blue<<endl;
+
+
+
     unsigned int new_color = 0;
     cout<<endl<<endl;
-    cout<<"New color: "<< setfill('0') << setw(6) << hex << new_color << endl;
+    cout<<"New color: " << hex << setfill('0') << setw(6) << new_color << endl;
 
     new_color <<= 8;
     new_color += red;
@@ -35,8 +41,8 @@ int main()
     new_color <<= 8;
     new_color += blue;
 
-    cout<<"Old color: "<< setfill('0') << setw(6) << hex << color << endl;
-    cout<<"New color: "<< setfill('0') << setw(6) << hex << new_color << endl;
+    cout<<"Old color: " << hex << color << endl;
+    cout<<"New color: " << hex << new_color << endl;
 
     return 0;
 }
