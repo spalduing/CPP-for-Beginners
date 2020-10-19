c7916e5a7736153dde5bef169a9cbbc80fb18584
Miguelangel Diaz <spalduing12@gmail.com>
2020-10-19 12:10:47 -0500

    Realistic Particle Motion, Pit of Souls

--- CPP_Beginners_Course/Particle_Fire_Explosion/src/Particle.cpp
+++ CPP_Beginners_Course/Particle_Fire_Explosion/src/Particle.cpp
@@ -6,10 +6,10 @@ namespace spalduing
 
 Particle::Particle(): m_x(0),
                       m_y(0),
-                      m_speed(0.001 * rand()/RAND_MAX),
+                      m_speed(0.04 * rand()/RAND_MAX),
                       m_direction( (2.0 *M_PI * rand())/RAND_MAX )
 {
-
+    m_speed *= m_speed;
 }
 
 Particle::~Particle()
@@ -19,6 +19,9 @@ Particle::~Particle()
 
 void Particle::update(int interval)
 {
+    //m_direction += interval*4; /*psicodelico hermano*/
+    //m_direction += interval*0.04; /*rasengan*/
+    m_direction += interval*0.0003; /*Pit of souls*/
     double xspeed = m_speed * cos(m_direction);
     double yspeed = m_speed * sin(m_direction);
 
