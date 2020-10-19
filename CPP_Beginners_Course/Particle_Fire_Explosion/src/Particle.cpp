f9c4ce90de8844ac98a09ad8ed1bbe9d36729aa6
Miguelangel Diaz <spalduing12@gmail.com>
2020-10-19 12:28:54 -0500

    73. Realistic Particle Motion (little thig fixed)

--- CPP_Beginners_Course/Particle_Fire_Explosion/src/Particle.cpp
+++ CPP_Beginners_Course/Particle_Fire_Explosion/src/Particle.cpp
@@ -16,8 +16,8 @@ void Particle::init()
 {
     m_x = 0;
     m_y = 0;
-    m_speed = 0.04 * rand()/RAND_MAX;
-    m_direction = (2.0 *M_PI * rand())/RAND_MAX;
+    m_speed = (0.04 * rand())/RAND_MAX;
+    m_direction = (2.0 * M_PI * rand())/RAND_MAX;
 
     m_speed *= m_speed;
 }
